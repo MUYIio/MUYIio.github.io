39ddd8ace6b0236272d06d9739de1e4375630e9c
Yshawlon <MUYIio@outlook.com>
2022-06-17 22:10:56 +0800

    Site updated: 2022-06-17 22:10:54

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://www.yshawlon.cn/live2dapi/",
-			cdnPath: "https://www.yshawlon.cn/live2dapi/"
+			apiPath: "https://www.yshawlon.cn/live2dapi/",
+			//cdnPath: "https://www.yshawlon.cn/live2dapi/"
 		});
 	});
 }
